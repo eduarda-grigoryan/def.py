a5e5396c5988ccd85bf4d913020f0cd6725e1589
eduarda-grigoryan <162128247+eduarda-grigoryan@users.noreply.github.com>
2024-03-04 16:35:56 +0400

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,75 @@
+#1
+#def my_sum(n1,n2):
+#   return n1+n2
+#print(my_sum(5,6))    
+
+#3
+#def midle(a,b,c):
+ # return (a+b+c)/3
+#print(midle(30,2,1))  
+
+#4
+#def math(a,b):
+#   return a+b,a-b,a/b,a*b,a**b,a//b
+#print(math(5,8)) 
+
+#5
+#def my_up(mstr):
+#    upper_st=""
+   # for el in mstr:
+   #     if 'a'<= el <= 'z':
+   #        upper_el=chr(ord(el)-32)  
+   #     else:
+   #         upper_el = el  
+   #     upper_st += upper_el
+   # return upper_st
+    
+#print(my_up("hello world"))  
+
+#6
+#def my_low(mstr):
+   # m_low = ""
+   # for el in mstr:
+    #    if 'A' <= el <= 'Z':
+     #       low_el = chr(ord(el) + 32)
+     #   else:
+     #       low_el = el  
+      #  m_low += low_el
+    #return m_low
+#print(my_low("HELLO"))
+
+#7
+#def firstupp(tmp):
+ #   f_tmp = ""
+ ##  for el in tmp:
+  #      if el == ' ':
+   #         is_first = True
+   #         f_tmp += el
+   #     elif is_first and 'a' <= el <= 'z':
+   #         f_el = chr(ord(el) - 32)
+   #         f_tmp += f_el
+   #         is_first = False
+    #    else:
+    #        f_tmp += el
+    #        is_first = False
+  #  return f_tmp
+
+#print(firstupp("hello my firend")) 
+
+#8reverse
+#def my_riverse(mstr):
+ #   tmp=""
+  #  for el in mstr:
+  #      tmp=el+tmp
+   # return tmp
+#print(my_riverse("hello word"))   
+
+#10longest word
+#def long_w(mstr):
+ #   el=mstr.split()
+  #  longest=max(el,key=len)
+  #  return longest
+#print(long_w("hello my good worldddd"))    
+
+
+    
